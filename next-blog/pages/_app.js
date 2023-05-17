035f7a3ad726a2346c480f74af7305ac15f1c779
efoneli <abbaefoneli@gmail.com>
2023-05-17 14:08:17 +0100

    icon

--- next-blog/pages/_app.js
+++ next-blog/pages/_app.js
@@ -1,9 +1,13 @@
 import NavBar from "./NavBar";
+import Head from "next/head";
 import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
   return (
     <>
+    <Head>
+        <link rel="icon" href="favicon.ico" />
+    </Head>
       <header>
         <NavBar />
       </header>
