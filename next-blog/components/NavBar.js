4dfdd2bdc2dc16f50cc516f861fa3257e260f650
efoneli <abbaefoneli@gmail.com>
2023-05-22 10:09:52 +0100

    today

--- next-blog/components/NavBar.js
+++ next-blog/components/NavBar.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ThemeSwitch from './ThemeSwitch'
 
 function NavBar() {
   return (
@@ -12,8 +13,14 @@ function NavBar() {
             <Link href="/about">About</Link>
           </li>
         </ul>
+        <ThemeSwitch />
         <style jsx>
           {`
+
+          nav {
+            display: flex;
+            justify-content: center;
+          }
             ul {
               list-style-type: none;
               padding: 0;
